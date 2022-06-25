ffcc7993fff85318a0e9159c29f544e1afe0c428
RenFukatsu <fukatsuren88@gmail.com>
2022-06-25 22:34:24 +0900

    [log]submit abc064 a

--- abc064/src/bin/a.rs
+++ abc064/src/bin/a.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        r: i8,
+        g: i8,
+        b: i8,
+    }
+
+    let x: i32 = r as i32 * 100 + g as i32 * 10 + b as i32;
+    if x % 4 == 0 {
+        println!("YES");
+    } else {
+        println!("NO");
+    }
 }
