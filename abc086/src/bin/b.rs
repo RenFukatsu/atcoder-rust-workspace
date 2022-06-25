77b69021cab2c044b191223c01909bcf4e849753
RenFukatsu <fukatsuren88@gmail.com>
2022-06-25 23:06:29 +0900

    [log]submit abc086 b

--- abc086/src/bin/b.rs
+++ abc086/src/bin/b.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: String,
+        b: String,
+    }
+
+    let x_str = a + &b;
+    let x = x_str.parse().unwrap();
+
+    let mut ans = "No";
+    for i in 0..1001 {
+        if i * i == x {
+            ans = "Yes";
+        }
+    }
+    println!("{}", ans);
 }
