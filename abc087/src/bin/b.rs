3c37336c3ef1a713a3e5218d19e9725d142da8f7
RenFukatsu <fukatsuren88@gmail.com>
2022-06-26 22:52:03 +0900

    [log]submit abc086 b

--- abc087/src/bin/b.rs
+++ abc087/src/bin/b.rs
@@ -1,3 +1,25 @@
+use im_rc::HashSet;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: i32,
+        b: i32,
+        c: i32,
+    }
+
+    let aval: i32 = 500;
+    let bval: i32 = 100;
+    let cval: i32 = 50;
+
+    let mut st: HashSet<i32>;
+    for i in 0..a {
+        for j in 0..b {
+            for k in 0..c {
+                st.insert(aval * i + bval * j + cval * k);
+            }
+        }
+    }
+
+    println!("{}", st.len());
 }
