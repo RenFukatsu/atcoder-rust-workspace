fb5128d4b2e952c8f0bbba3cb352465e43d4e703
RenFukatsu <fukatsuren88@gmail.com>
2022-06-26 23:09:50 +0900

    [log]submit abc087 b

--- abc083/src/bin/b.rs
+++ abc083/src/bin/b.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+        a: i32,
+        b: i32,
+    }
+
+    let mut ans = 0;
+    for i in 1..n+1 {
+        let stri = i.to_string();
+        let mut sum = 0;
+        for c in stri.chars() {
+            let num = c as i32 - '0' as i32;
+            sum += num;
+        }
+        if a <= sum && sum <= b {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
--- abc087/src/bin/b.rs
+++ abc087/src/bin/b.rs
@@ -1,4 +1,3 @@
-use im_rc::HashSet;
 use proconio::input;
 
 fn main() {
@@ -6,20 +5,23 @@ fn main() {
         a: i32,
         b: i32,
         c: i32,
+        x: i32,
     }
 
     let aval: i32 = 500;
     let bval: i32 = 100;
     let cval: i32 = 50;
 
-    let mut st: HashSet<i32>;
-    for i in 0..a {
-        for j in 0..b {
-            for k in 0..c {
-                st.insert(aval * i + bval * j + cval * k);
+    let mut ans = 0;
+    for i in 0..a+1 {
+        for j in 0..b+1 {
+            for k in 0..c+1 {
+                if aval * i + bval * j + cval * k == x {
+                    ans += 1;
+                }
             }
         }
     }
 
-    println!("{}", st.len());
+    println!("{}", ans);
 }
