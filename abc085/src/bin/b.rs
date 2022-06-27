85285fdf10bbe82d0cdaebc29eeab4de7cb7f215
RenFukatsu <fukatsuren88@gmail.com>
2022-06-27 20:45:11 +0900

    [log]submit abc085 b

--- abc085/src/bin/b.rs
+++ abc085/src/bin/b.rs
@@ -1,3 +1,11 @@
+use im_rc::HashSet;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        ds: [i32; n],
+    }
+    let st: HashSet<i32> = ds.into_iter().collect();
+    println!("{}", st.len());
 }
