d45190e7c399fb021db48905275414067086a3b1
RenFukatsu <fukatsuren88@gmail.com>
2022-06-27 21:47:53 +0900

    [log]submit abc049 c

--- abc049/src/bin/c.rs
+++ abc049/src/bin/c.rs
@@ -1,3 +1,31 @@
+use itertools::Itertools;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        s: String,
+    }
+    let revs = s.chars().into_iter().rev().collect::<String>();
+    let dre = ["dream", "dreamer", "erase", "eraser"];
+    let revdre = dre.iter().map(|x| x.to_string().chars().rev().collect::<String>()).collect_vec();
+
+    let mut i = 0;
+    let mut ans = "YES";
+    loop {
+        let research = &revs[i..std::cmp::min(revs.len(), i+7)];
+        let prei = i;
+        for r in &revdre {
+            if research.contains(r) {
+                i += r.len();
+            }
+        }
+        if prei == i {
+            ans = "NO";
+            i += 1;
+        }
+        if i >= revs.len() {
+            break;
+        }
+    }
+    println!("{}", ans);
 }
