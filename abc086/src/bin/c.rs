8987e1df3a6ade1d3e1038515013fb51fd6668bf
RenFukatsu <fukatsuren88@gmail.com>
2022-06-27 22:22:41 +0900

    [log]submit abc086 c

--- abc086/src/bin/c.rs
+++ abc086/src/bin/c.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        txys: [(i32, i32, i32); n],
+    }
+
+    let mut pret = 0;
+    let mut prex = 0;
+    let mut prey = 0;
+    let mut ans = "Yes";
+    for (t, x, y) in txys {
+        let dt = t - pret;
+        let dist = (x - prex).abs() + (y - prey).abs();
+        if dt >= dist && (dt - dist) % 2 == 0 {
+            pret = t;
+            prex = x;
+            prey = y;
+            continue;
+        }
+        ans = "No";
+        break;
+    }
+
+    println!("{}", ans);
 }
