7e05b38ac4317115ba682ed1f61fb88a8089eee3
RenFukatsu <fukatsuren88@gmail.com>
2022-06-27 23:30:02 +0900

    [log]submit typical90 002

--- typical90/src/bin/002.rs
+++ typical90/src/bin/002.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+    }
+    let mut ans = Vec::<String>::new();
+    if n % 2 == 0 {
+        search(0, &n, 0, String::from(""), &mut ans);
+    }
+    for a in ans {
+        println!("{}", a);
+    }
+}
+
+fn search(i: i32, n: &i32, left: i32, s: String, ans: &mut Vec::<String>) {
+    if i == *n {
+        ans.push(s);
+        return;
+    }
+
+    if left != *n - i {
+        let mut sc = s.clone();
+        sc.push('(');
+        search(i + 1, n, left + 1, sc, ans);
+    }
+    if left > 0 {
+        let mut sc = s.clone();
+        sc.push(')');
+        search(i + 1, n, left - 1, sc, ans);
+    }
 }
