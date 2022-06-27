349aa1bad1b0c2cbb7f163931324e1b3de3cda91
RenFukatsu <fukatsuren88@gmail.com>
2022-06-27 19:44:32 +0900

    [log]submit abc088 b

--- abc088/src/bin/b.rs
+++ abc088/src/bin/b.rs
@@ -1,3 +1,23 @@
+use std::cmp::Reverse;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut a: [i32; n],
+    }
+
+    a.sort_by_key(|&x| Reverse(x));
+
+    let mut alice = 0;
+    let mut bob = 0;
+    for (i, val) in a.iter().enumerate() {
+        if i % 2 == 0 {
+            alice += val;
+        } else {
+            bob += val;
+        }
+    }
+    println!("{}", alice - bob);
 }
