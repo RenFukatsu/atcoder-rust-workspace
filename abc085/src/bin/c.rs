08690d90eb126e8e70c8af1fc003f9344f698de6
RenFukatsu <fukatsuren88@gmail.com>
2022-06-27 21:03:11 +0900

    [log]submit abc085 c

--- abc085/src/bin/c.rs
+++ abc085/src/bin/c.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+        y: i32,
+    }
+    let mut ans = (-1, -1, -1);
+    for i in 0..n + 1 {
+        for j in 0..n - i + 1 {
+            let k = n - i - j;
+            if i * 10000 + j * 5000 + k * 1000 == y {
+                ans = (i, j, k);
+            }
+        }
+    }
+    println!("{} {} {}", ans.0, ans.1, ans.2);
 }
