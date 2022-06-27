032b7393f07aa49980567d6394bcfe88ea1a047e
RenFukatsu <fukatsuren88@gmail.com>
2022-06-27 22:57:58 +0900

    [log]submit typical90 001

--- typical90/src/bin/001.rs
+++ typical90/src/bin/001.rs
@@ -1,3 +1,30 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+        l: i32,
+        k: i32,
+        mut alist: [i32; n],
+    }
+    alist.push(l);
+    let mut ng = l;
+    let mut ok = 1;
+    while ng - ok > 1 {
+        let x = (ng + ok) / 2;
+        let mut start = 0;
+        let mut count = 0;
+        for a in &alist {
+            if a - start >= x {
+                count += 1;
+                start = *a;
+            }
+        }
+        if count > k {
+            ok = x;
+        } else {
+            ng = x;
+        }
+    }
+    println!("{}", ok);
 }
