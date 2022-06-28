52f5bd08f9f7745a4f869de7df2d0956a073e8ab
RenFukatsu <fukatsuren88@gmail.com>
2022-06-28 19:05:29 +0900

    [log]submit typical90 004

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        h: usize,
+        w: usize,
+        mp: [[usize; w]; h],
+    }
+    let mut sum_row = vec![0_usize; h];
+    let mut sum_col = vec![0_usize; w];
+    for i in 0..h {
+        for j in 0..w {
+            sum_row[i] += mp[i][j];
+            sum_col[j] += mp[i][j];
+        }
+    }
+
+    for i in 0..h {
+        for j in 0..w {
+            if j != 0 {
+                print!(" ");
+            }
+            print!("{}", sum_row[i] + sum_col[j] - mp[i][j]);
+        }
+        println!();
+    }
 }
