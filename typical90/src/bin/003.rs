18b3ada3f50be896a9be5dac2daeb22a79295bf6
RenFukatsu <fukatsuren88@gmail.com>
2022-06-28 18:25:59 +0900

    [log]submit typical90 003

--- typical90/src/bin/003.rs
+++ typical90/src/bin/003.rs
@@ -1,3 +1,37 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        ab: [(usize, usize); n-1],
+    }
+    let mut mp = vec![Vec::<usize>::new(); n];
+    for (a, b) in ab {
+        let a = a - 1;
+        let b = b - 1;
+        mp[a].push(b);
+        mp[b].push(a);
+    }
+    let mut ans = 0;
+    search(0, 0, &mp, &mut ans);
+    println!("{}", ans + 1);
+}
+
+fn search(pre: usize, now: usize, mp: &Vec<Vec<usize>>, ans: &mut i32) -> i32 {
+    let mut mlen = 0;
+    let mut m2len = 0;
+    for next in &mp[now] {
+        if pre == *next {
+            continue;
+        }
+        let len = search(now, *next, mp, ans);
+        if len > mlen {
+            m2len = mlen;
+            mlen = len;
+        } else if len > m2len {
+            m2len = len;
+        }
+    }
+    *ans = std::cmp::max(*ans, mlen + m2len);
+    mlen + 1
 }
