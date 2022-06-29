ecdd869a78ada96121a1e9bd39bd7c724f099ec4
RenFukatsu <fukatsuren88@gmail.com>
2022-06-29 21:43:54 +0900

    [log]submit typical90 005

--- typical90/src/bin/005.rs
+++ typical90/src/bin/005.rs
@@ -1,3 +1,43 @@
+use proconio::input;
+
+// 1点解法
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        b: usize,
+        k: usize,
+        c: [usize; k],
+    }
+    let mut contain = [false; 10];
+    for val in c {
+        contain[val] = true;
+    }
+    let contain = contain;
+    // eprintln!("contain = {:?}", contain);
+    let modulo: usize = 1000000007;
+    let mut ans = 0;
+    let mut num = b;
+    while num.to_string().len() < n {
+        num += b;
+    }
+    while num.to_string().len() < n + 1 {
+        let mut div = 1;
+        for _ in 1..num.to_string().len() {
+            div *= 10;
+        }
+        let mut ok = true;
+        while div > 0 {
+            let i = num % (div * 10) / div;
+            if !contain[i] {
+                ok = false;
+            }
+            div /= 10;
+        }
+        // eprintln!("num = {}, div = {}, ok = {}", num, div, ok);
+        if ok {
+            ans += 1;
+        }
+        num += b;
+    }
+    println!("{}", ans % modulo);
 }
