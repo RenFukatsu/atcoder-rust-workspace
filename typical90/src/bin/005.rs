9eff7bf3d5ab4761b965287ffe133608ad0ba643
RenFukatsu <fukatsuren88@gmail.com>
2022-07-06 19:47:21 +0900

    [log]init abs

--- typical90/src/bin/005.rs
+++ typical90/src/bin/005.rs
@@ -4,26 +4,9 @@ const MOD: u32 = 1000000007;
 
 fn main() {
     input! {
-        n: usize,
-        b: usize,
+        digit: usize,
+        div: usize,
         k: usize,
-        cs: [usize; k],
+        nums: [usize; k],
     }
-    let mut rems = vec![0; b];
-    for c in &cs {
-        rems[c % b] += 1;
-    }
-    for _ in 0..n-1 {
-        let mut nrems = vec![0; b];
-        for (i, rem) in rems.iter().enumerate() {
-            for c in &cs {
-                let idx = (i * 10 + c) % b;
-                nrems[idx] += *rem;
-                nrems[idx] %= MOD;
-            }
-        }
-        rems = nrems;
-        // eprintln!("{:?}", rems);
-    }
-    println!("{}", rems[0]);
 }
