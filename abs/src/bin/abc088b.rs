d066d5c015d29bcaa9d0e071c55d143fd61d06cf
RenFukatsu <fukatsuren88@gmail.com>
2022-07-06 20:21:27 +0900

    [log]submit abs abc088b

--- abs/src/bin/abc088b.rs
+++ abs/src/bin/abc088b.rs
@@ -1,3 +1,23 @@
+use itertools::Itertools;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut alist: [usize; n],
+    }
+
+    alist.sort_by(|a, b| b.cmp(a));
+
+    let mut alice = 0;
+    let mut bob = 0;
+    for (i, a) in alist.iter().enumerate() {
+        if i % 2 == 0 {
+            alice += a;
+        } else {
+            bob += a;
+        }
+    }
+
+    println!("{}", alice - bob);
 }
