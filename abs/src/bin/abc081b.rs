f63a06c2ad3dc1693e6851518bf1d0cd4700c961
RenFukatsu <fukatsuren88@gmail.com>
2022-07-06 20:10:45 +0900

    [log]submit abs abc081b

--- abs/src/bin/abc081b.rs
+++ abs/src/bin/abc081b.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut alist: [usize; n],
+    }
+
+    let mut ans = 0;
+    loop {
+        let mut exist_odd = false;
+        for a in alist.iter_mut() {
+            if *a % 2 == 0 {
+                *a /= 2;
+            } else {
+                exist_odd = true;
+            }
+        }
+        if exist_odd {
+            break;
+        }
+        ans += 1;
+    }
+    println!("{}", ans);
 }
