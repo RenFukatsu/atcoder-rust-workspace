264f473c619d927583c77d5a6ad3b696d6336d2f
RenFukatsu <fukatsuren88@gmail.com>
2022-07-06 20:04:51 +0900

    [log]submit abs abc081a

--- abs/src/bin/abc081a.rs
+++ abs/src/bin/abc081a.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: String,
+    }
+
+    let mut ans = 0;
+    for c in n.chars() {
+        if c == '1' {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }
