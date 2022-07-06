1045f2301b39e14e4c59757bedd4619fdf1762a9
RenFukatsu <fukatsuren88@gmail.com>
2022-07-06 20:15:13 +0900

    [log]submit abs abc087b

--- abs/src/bin/abc087b.rs
+++ abs/src/bin/abc087b.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: usize,
+        b: usize,
+        c: usize,
+        x: usize,
+    }
+
+    let mut ans = 0;
+    for i in 0..a+1 {
+        for j in 0..b+1 {
+            for k in 0..c+1 {
+                if x == i * 500 + j * 100 + k * 50 {
+                    ans += 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
